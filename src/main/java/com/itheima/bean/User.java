edc2097bae09056ab223635c5c70a7c373876b8a
huangqi <1551094825@qq.com>
2021-07-20 23:48:26 +0800

    增信bean

---
+++ src/main/java/com/itheima/bean/User.java
@@ -0,0 +1,13 @@
+package com.itheima.bean;
+
+/**
+ * @ClassName User
+ * @Description TODO
+ * @Author HuangQi
+ * @Date 2021/7/20 0020 23:22
+ * @Version 1.0
+ **/
+public class User {
+    private String name;
+    private String email;
+}
